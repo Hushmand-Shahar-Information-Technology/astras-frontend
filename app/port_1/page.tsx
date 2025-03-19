0a7042c00b4ec2d12206aa09a5707b6c7f9e8370
Farhad Rahmani <55535348+farhadrahmanii@users.noreply.github.com>
2025-03-19 15:46:20 +0430

    Seperated pages is created for both Port1 and Port2

---
+++ app/port_1/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { PageContainer } from "@toolpad/core/PageContainer";
+
+export default function Dashboard() {
+  return <h1>Port 1</h1>;
+}
