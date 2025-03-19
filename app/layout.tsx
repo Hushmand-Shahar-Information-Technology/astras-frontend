627897ce78e46a8d5f12a6d3a7be943c66fc0ab7
Farhad Rahmani <55535348+farhadrahmanii@users.noreply.github.com>
2025-03-19 17:25:30 +0430

    د آمریت ټولی بټی اضاف شوی په هیډر د فرنټ انډ کی.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,9 @@ import { usePathname } from "next/navigation";
 import "./globals.css";
 import type { Metadata } from "next";
 import Sidebar from "./components/layout/sidebar";
-
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Box from "@mui/material/Box";
 // Import fonts
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,7 +40,37 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           {/* Main Content - Scrollable */}
           <main className="flex-1 overflow-hidden flex flex-col">
             <header className="p-8 bg-white shadow-sm z-10">
-              {pathname.split("/").pop()}
+              {/* {pathname.split("/").pop()} */}
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "end",
+                  justifyContent: "end",
+                  direction: "ltr",
+                  "& > *": {
+                    m: 0,
+                  },
+                }}
+              >
+                <ButtonGroup
+                  variant="contained"
+                  aria-label="Basic button group"
+                >
+                  <Button>آمریت خط استیشن</Button>
+                  <Button>آمریت پورت نمبر یک</Button>
+                  <Button>آمریت پورت نمبر دو</Button>
+                  <Button>آمریت پورت نمبر سه</Button>
+                  <Button>آمریت مالی</Button>
+                  <Button>آمریت کنترول</Button>
+                  <Button>آمریت خدمات</Button>
+                  <Button>آمریت پلان</Button>
+                  <Button>آمریت منابع بشری</Button>
+                  <Button>آمریت اسناد و ارتباط</Button>
+                  <Button>آمریت تجارتی</Button>
+                  <Button>آمریت تخنیک و ترانسپورت</Button>
+                </ButtonGroup>
+              </Box>
             </header>
             <div className="flex-1 p-8 overflow-y-auto bg-white">
               {children}
